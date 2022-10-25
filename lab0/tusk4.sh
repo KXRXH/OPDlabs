3b4b5febd62e9d3996afb70482aa2a222c1b2ed8
KXRXH <kxrxh.contact@gmail.com>
2022-10-25 09:47:20 +0300

    helios fix

--- lab0/tusk4.sh
+++ lab0/tusk4.sh
@@ -7,7 +7,7 @@ cd sneasel6 || exit
 wc -m ./* 2>/dev/null | sort -n
 # 2'd tusk
 cd ..
-ls -lR | sort -u | grep -x "[^\.]*" | grep -v '^$' | head -n 4
+ls -lR | sort -u -k 2 -nr | grep -v '^t\|^$' | head -n 4
 # 3'd tusk
 cd combee3 || exit
 cat -n ponyta | grep "[^h]$"
